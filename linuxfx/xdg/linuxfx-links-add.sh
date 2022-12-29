feda89f5ffadddeb0f09dcae2aee2edb7beb6ce5
Piero Proietti <piero.proietti@gmail.com>
2022-12-29 18:08:13 +0100

    xdg linuxfx

---
+++ linuxfx/xdg/linuxfx-links-add.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+DESKTOP=$(xdg-user-dir DESKTOP)
+# Create ~/Desktop just in case this runs before the xdg folder creation script
+mkdir -p $DESKTOP
+cp /usr/share/applications/penguins-eggs.desktop $DESKTOP
+cp /usr/share/applications/devicemanager.desktop $DESKTOP
+cp /usr/share/applications/microsoft-edge.desktop $DESKTOP
+# cp /usr/share/applications/trash.desktop $DESKTOP
+cp /usr/share/applications/windowsfx-android.desktop $DESKTOP
+chmod +x $DESKTOP/*.desktop
+
