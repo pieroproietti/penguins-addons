76a5eeed529efb6484058c2f5f5e2d1f5db9e676
Piero Proietti <piero.proietti@gmail.com>
2022-12-30 17:26:38 +0100

    fix

--- linuxfx/patch/apply.sh
+++ linuxfx/patch/apply.sh
@@ -1,5 +1,5 @@
-sudo cp linuxfx-links-add.sh /usr/bin
 sudo cp linuxfx-mkdesktop.sh /usr/bin
-cp linuxfx-links-add.desktop ~/.config/autostart
 cp linuxfx-mkdesktop.desktop ~/.config/autostart
+sudo cp linuxfx-links-add.sh /usr/bin
+cp linuxfx-links-add.desktop ~/.config/autostart
 cp /etc/xdg/autostart/penguins-links-add.desktop ~/.config/autostart
