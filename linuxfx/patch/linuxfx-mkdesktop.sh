38c1c6c28071e24bb6315d5a2cac7fa632fd3cbb
Piero Proietti <piero.proietti@gmail.com>
2022-12-30 15:36:45 +0100

    linuxfx

--- linuxfx/patch/linuxfx-mkdesktop.sh
+++ linuxfx/patch/linuxfx-mkdesktop.sh
@@ -1,11 +1,11 @@
+#!/bin/sh
 #
 # linuxfx-mkdesktop.sg
 # 
-if [ -f "~/.linuxfx"]; then
+if [ ! -d ~/.linuxfx ]; then
     xdg-user-dirs-update
     xdg-user-dirs-update --force
     penguins-eggs-links-add.sh
     linuxfx-links-add.sh
-    touch "~/.linuxfx"
+    touch ~/.linuxfx
 fi
-
