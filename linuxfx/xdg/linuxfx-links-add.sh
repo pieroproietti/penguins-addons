aa4e6b95572bd80b18b2697c5520cf0681494d25
Piero Proietti <piero.proietti@gmail.com>
2022-12-30 12:06:56 +0100

    linuxfx

--- linuxfx/xdg/linuxfx-links-add.sh
+++ linuxfx/xdg/linuxfx-links-add.sh
@@ -5,7 +5,6 @@ mkdir -p $DESKTOP
 cp /usr/share/applications/penguins-eggs.desktop $DESKTOP
 cp /usr/share/applications/devicemanager.desktop $DESKTOP
 cp /usr/share/applications/microsoft-edge.desktop $DESKTOP
-# cp /usr/share/applications/trash.desktop $DESKTOP
+cp /opt/Linuxfx/helloa/trash.desktop $DESKTOP
 cp /usr/share/applications/windowsfx-android.desktop $DESKTOP
 chmod +x $DESKTOP/*.desktop
-
