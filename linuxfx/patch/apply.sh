100f344b32a6b360d2c60929699e2f5e3a029650
Piero Proietti <piero.proietti@gmail.com>
2022-12-30 17:24:16 +0100

    fix

--- linuxfx/patch/apply.sh
+++ linuxfx/patch/apply.sh
@@ -1,4 +1,5 @@
 sudo cp linuxfx-links-add.sh /usr/bin
 sudo cp linuxfx-mkdesktop.sh /usr/bin
 cp linuxfx-links-add.desktop ~/.config/autostart
-#cp penguins-links-add.desktop ~/.config/autostart
+cp linuxfx-mkdesktop.desktop ~/.config/autostart
+cp /etc/xdg/autostart/penguins-links-add.desktop ~/.config/autostart
