881f64c40ff190c47d4ead5af2aeeb583c33818c
Piero Proietti <piero.proietti@gmail.com>
2022-12-30 17:05:24 +0100

    linuxfx

--- linuxfx/patch/apply.sh
+++ linuxfx/patch/apply.sh
@@ -1,4 +1,4 @@
 sudo cp linuxfx-links-add.sh /usr/bin
 sudo cp linuxfx-mkdesktop.sh /usr/bin
 cp linuxfx-links-add.desktop ~/.config/autostart
-cp penguins-links-add.desktop ~/.config/autostart
+#cp penguins-links-add.desktop ~/.config/autostart
--- linuxfx/patch/linuxfx-mkdesktop.sh
+++ linuxfx/patch/linuxfx-mkdesktop.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ ! -f ~/.linuxfx ]; then
+if [ ! -f ~/.linuxfx-eggs ]; then
     xdg-user-dirs-update
     xdg-user-dirs-update --force
     penguins-links-add.sh
     linuxfx-links-add.sh
-    touch ~/.linuxfx
+    touch ~/.linuxfx-eggs
 fi
 
