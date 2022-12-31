7218dac748bdd5bfd9e0bbe9734e666eb7f50886
Piero Proietti <piero.proietti@gmail.com>
2022-12-31 08:16:25 +0100

    linuxfx

--- linuxfx/patch/apply.sh
+++ linuxfx/patch/apply.sh
@@ -1,5 +1,11 @@
 sudo cp linuxfx-mkdesktop.sh /usr/bin
 cp linuxfx-mkdesktop.desktop ~/.config/autostart
+sudo cp linuxfx-mkdesktop.desktop /etc/xdg/autostart
+sudo cp linuxfx-mkdesktop.desktop /usr/share/applications
+
 sudo cp linuxfx-links-add.sh /usr/bin
 cp linuxfx-links-add.desktop ~/.config/autostart
+sudo cp linuxfx-links-add.desktop /etc/xdg/autostart
+sudo cp linuxfx-links-add.desktop /usr/share/applications
+
 cp /etc/xdg/autostart/penguins-links-add.desktop ~/.config/autostart
