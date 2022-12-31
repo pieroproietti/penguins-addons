617091a683a8151203bc68edc3af1751e29c30cc
Piero Proietti <piero.proietti@gmail.com>
2022-12-31 19:21:18 +0100

    added xdg-user-dirs

--- linuxfx/patch/apply.sh
+++ linuxfx/patch/apply.sh
@@ -8,4 +8,8 @@ cp linuxfx-links-add.desktop ~/.config/autostart
 sudo cp linuxfx-links-add.desktop /etc/xdg/autostart
 sudo cp linuxfx-links-add.desktop /usr/share/applications
 
+cp xdg-user-dirs-update.desktop ~/.config/autostart
+sudo cp xdg-user-dirs-update.desktop /etc/xdg/autostart
+sudo cp xdg-user-dirs-update.desktop /usr/share/applications
+
 cp /etc/xdg/autostart/penguins-links-add.desktop ~/.config/autostart
--- linuxfx/patch/linuxfx-mkdesktop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -f ~/.linuxfx-eggs ]; then
-    xdg-user-dirs-update > /dev/null
-    xdg-user-dirs-update --force > /dev/null
-    penguins-links-add.sh > /dev/null
-    linuxfx-links-add.sh > /dev/null
-    touch ~/.linuxfx-eggs > /dev/null
-fi
-
